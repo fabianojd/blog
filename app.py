b8fd904c41f73377dc2ceef96f92b3f8e9675945
fabianojd <fabianojd@yahoo.com.br>
2022-07-25 23:50:16 +0000

    Atualização 2

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask("")
 @app.route("/")
 def hello():
     return "Olá Mundo"
+
+@app.route("/meucontato")
+def meuContato():
+    return """<html>
+    <head>
+        <title>Contatos</title>
+    </head>
+    <body>
+    <p>Fabiano Jorge Dornelles, fabianojd@yahoo.com.br, Fone XXXXXX<p>
+    </body>
+    </html>"""
