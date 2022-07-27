e30cd05100e6a812213595f353c50111514600a3
fabianojd <fabianojd@yahoo.com.br>
2022-07-27 04:32:06 +0000

    Atualização projeto 26/07/2002
     Changes to be committed:
    	new file:   .gitignore
    	modified:   __pycache__/app.cpython-38.pyc
    	modified:   app.py
    	new file:   static/style.css
    	new file:   templates/base.html
    	modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask("")
+app = Flask("hello")
+
+posts = [
+    {
+        "title": "O meu Primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Fabiano",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Fabiano",
+        "created": datetime(2022,7,26)
+    },
+]
 
 @app.route("/")
-def hello():
-    return "Olá Mundo"
-
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+def index():
+    return render_template("index.html", posts=posts)
